348e56b88c79df0aee07e609dbb8a02a7273fbfd
RUDRA JAAT <136905244+RUDRA-JAAT@users.noreply.github.com>
2023-10-17 07:20:36 +0530

    Update raid.py

--- Rudra/plugins/raid.py
+++ Rudra/plugins/raid.py
@@ -2,7 +2,7 @@ import asyncio
 
 from random import choice
 
-from pyrogram.type import events
+from pyrogram.types import events
 
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
 from Rudra.data import RAID, REPLYRAID, Rudra, MRAID, SRAID, CRAID, Rudra
